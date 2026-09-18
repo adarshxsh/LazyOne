80aab5d7b164662a874831758a4a57d2c4babac6
Adarsh <udhabhaw@gmail.com>
2026-09-18 19:25:00 +0000

    feat: proportional task collateral lockup with escrow and ledger slashing

--- basic/models.py
+++ basic/models.py
@@ -52,6 +52,10 @@ class Task(models.Model):
         return self.title
 
     @property
+    def collateral_amount(self):
+        return self.reward // 2
+
+    @property
     def main_chat(self):
         return self.conversations.first()
 
@@ -68,6 +72,10 @@ class RewardLedger(models.Model):
         ('dispute_deposit', 'Dispute Deposit Bond Held'),
         ('dispute_refund', 'Dispute Deposit Bond Refunded'),
         ('dispute_forfeit', 'Dispute Deposit Bond Forfeited'),
+        ('collateral_lock', 'Collateral Lock (Points Locked)'),
+        ('collateral_release', 'Collateral Release (Points Returned)'),
+        ('collateral_refund', 'Collateral Refund (Points Returned)'),
+        ('collateral_slash', 'Collateral Slash (Penalty Deducted)'),
     )
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reward_transactions')
     task = models.ForeignKey(Task, on_delete=models.SET_NULL, null=True, blank=True)
