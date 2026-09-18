0b2b1dda4e41e22c84d57ffdfc4accfd04e0f60e
Adarsh <udhabhaw@gmail.com>
2026-09-18 19:23:24 +0000

    Implement dispute state guarding and direct ledger penalty logging for task abandonment

--- basic/views/dispute.py
+++ basic/views/dispute.py
@@ -24,8 +24,11 @@ def raise_dispute(request, task_id):
     task = get_object_or_404(Task, id=task_id)
     if hasattr(task, 'dispute') and task.dispute.status == 'open':
         return redirect('dispute_detail', dispute_id=task.dispute.id)
-    if task.taken_by != request.user or task.status != 'in_progress':
-        messages.error(request, "You can only raise a dispute for a task you have taken that is currently in progress.")
+    if request.user != task.posted_by and request.user != task.taken_by:
+        messages.error(request, "You can only raise a dispute for a task you posted or took.")
+        return redirect('my_tasks')
+    if task.status != 'in_progress':
+        messages.error(request, "You can only raise a dispute for a task that is currently in progress.")
         return redirect('my_tasks')
     if request.method == 'POST':
         reason = request.POST.get('reason')
@@ -74,11 +77,13 @@ def raise_dispute(request, task_id):
             task.status = 'disputed'
             task.save()
 
-            Notification.objects.create(
-                recipient=task.posted_by,
-                message=f"{request.user.username} has raised a dispute for your task: '{task.title}'.",
-                link=reverse('dispute_detail', args=[dispute.id])
-            )
+            other_user = task.taken_by if request.user == task.posted_by else task.posted_by
+            if other_user:
+                Notification.objects.create(
+                    recipient=other_user,
+                    message=f"{request.user.username} has raised a dispute for your task: '{task.title}'.",
+                    link=reverse('dispute_detail', args=[dispute.id])
+                )
         messages.success(request, f"Dispute raised successfully. {deposit_amount} points held as deposit bond.")
         return redirect('dispute_detail', dispute_id=dispute.id)
     return redirect('my_tasks')
@@ -98,10 +103,12 @@ def withdraw_dispute(request, dispute_id):
         task.status = 'in_progress'
         task.save()
 
-        Notification.objects.create(
-            recipient=task.posted_by,
-            message=f"{request.user.username} has withdrawn the dispute for '{task.title}'. The task is now in progress.",
-            link=reverse('my_tasks')
-        )
+        other_user = task.taken_by if request.user == task.posted_by else task.posted_by
+        if other_user:
+            Notification.objects.create(
+                recipient=other_user,
+                message=f"{request.user.username} has withdrawn the dispute for '{task.title}'. The task is now in progress.",
+                link=reverse('my_tasks')
+            )
     messages.success(request, f"You have successfully withdrawn the dispute for '{task.title}'. Your deposit bond has been refunded.")
     return redirect('my_tasks')
