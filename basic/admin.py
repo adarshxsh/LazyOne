2631ab36376ab2ab8712735030960e5ffd4a511b
Adarsh <udhabhaw@gmail.com>
2026-09-18 22:06:15 +0000

    Implement relational DisputeEvidence model with multi-file attachment endpoints

--- basic/admin.py
+++ basic/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Dispute, DisputeEvidence, Task, UserProfile, RewardLedger
 
-# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Task)
+admin.site.register(Dispute)
+admin.site.register(DisputeEvidence)
+admin.site.register(RewardLedger)
