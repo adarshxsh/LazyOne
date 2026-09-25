4875610009300a844a5818f0cc717115328ea3c8
Adarsh <udhabhaw@gmail.com>
2026-09-25 01:12:50 +0000

    feat(dispute): build automated social graph filtered juror selection engine

--- basic/tests.py
+++ basic/tests.py
@@ -182,3 +182,149 @@ class DisputeDepositBondTests(TestCase):
         forfeit_ledger = RewardLedger.objects.filter(user=self.taker, transaction_type='dispute_forfeit').first()
         self.assertIsNotNone(forfeit_ledger)
 
+
+from .models import JurorAssignment, Friendship, Notification
+
+class JurorSelectionTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+        self.poster = User.objects.create_user(username='poster_user', password='password123')
+        self.poster_profile = UserProfile.objects.create(user=self.poster, rewards=1000)
+
+        self.taker = User.objects.create_user(username='taker_user', password='password123')
+        self.taker_profile = UserProfile.objects.create(user=self.taker, rewards=1000)
+
+        self.task = Task.objects.create(
+            title="Juror Test Task",
+            description="Task for testing juror selection",
+            reward=200,
+            posted_by=self.poster,
+            taken_by=self.taker,
+            status='in_progress',
+            deadline=timezone.now() + timedelta(days=2)
+        )
+        Conversation.objects.create(task=self.task)
+
+    def test_auto_juror_selection_on_dispute_creation(self):
+        # Create 5 neutral community members
+        community_members = []
+        for i in range(5):
+            u = User.objects.create_user(username=f'neutral_{i}', password='password123')
+            UserProfile.objects.create(user=u, rewards=500)
+            community_members.append(u)
+
+        self.client.login(username='taker_user', password='password123')
+        response = self.client.post(
+            reverse('raise_dispute', args=[self.task.id]),
+            {'reason': 'Incomplete work'}
+        )
+
+        dispute = Dispute.objects.get(task=self.task)
+        self.assertRedirects(response, reverse('dispute_detail', args=[dispute.id]))
+
+        # Verify exactly 3 juror assignments created
+        assignments = JurorAssignment.objects.filter(dispute=dispute)
+        self.assertEqual(assignments.count(), 3)
+
+        assigned_user_ids = set(assignments.values_list('juror_id', flat=True))
+        # Direct task creator and taker must not be assigned
+        self.assertNotIn(self.poster.id, assigned_user_ids)
+        self.assertNotIn(self.taker.id, assigned_user_ids)
+
+        # Verify notifications created for assigned jurors
+        for juror_id in assigned_user_ids:
+            notif = Notification.objects.filter(recipient_id=juror_id, link=reverse('dispute_detail', args=[dispute.id])).first()
+            self.assertIsNotNone(notif)
+            self.assertIn("assigned as a juror", notif.message)
+
+    def test_social_graph_exclusion_friends_and_closeness(self):
+        # Friend of poster
+        friend_poster = User.objects.create_user(username='friend_poster', password='password123')
+        fp_profile = UserProfile.objects.create(user=friend_poster, rewards=500)
+        self.poster_profile.friends.add(fp_profile)
+
+        # Friend of taker
+        friend_taker = User.objects.create_user(username='friend_taker', password='password123')
+        ft_profile = UserProfile.objects.create(user=friend_taker, rewards=500)
+        self.taker_profile.friends.add(ft_profile)
+
+        # High closeness with poster (closeness = 80)
+        close_poster = User.objects.create_user(username='close_poster', password='password123')
+        cp_profile = UserProfile.objects.create(user=close_poster, rewards=500)
+        Friendship.objects.create(from_user=self.poster_profile, to_user=cp_profile, closeness=80)
+
+        # High closeness with taker (closeness = 50)
+        close_taker = User.objects.create_user(username='close_taker', password='password123')
+        ct_profile = UserProfile.objects.create(user=close_taker, rewards=500)
+        Friendship.objects.create(from_user=ct_profile, to_user=self.taker_profile, closeness=50)
+
+        # Create 3 neutral users
+        neutral1 = User.objects.create_user(username='neutral_1', password='password123')
+        UserProfile.objects.create(user=neutral1, rewards=500)
+        neutral2 = User.objects.create_user(username='neutral_2', password='password123')
+        UserProfile.objects.create(user=neutral2, rewards=500)
+        neutral3 = User.objects.create_user(username='neutral_3', password='password123')
+        UserProfile.objects.create(user=neutral3, rewards=500)
+
+        self.client.login(username='taker_user', password='password123')
+        self.client.post(
+            reverse('raise_dispute', args=[self.task.id]),
+            {'reason': 'Quality dispute'}
+        )
+
+        dispute = Dispute.objects.get(task=self.task)
+        assignments = JurorAssignment.objects.filter(dispute=dispute)
+        self.assertEqual(assignments.count(), 3)
+
+        assigned_user_ids = set(assignments.values_list('juror_id', flat=True))
+
+        # None of the excluded users should be assigned
+        excluded_ids = {
+            self.poster.id,
+            self.taker.id,
+            friend_poster.id,
+            friend_taker.id,
+            close_poster.id,
+            close_taker.id
+        }
+        for ex_id in excluded_ids:
+            self.assertNotIn(ex_id, assigned_user_ids)
+
+        # The 3 assigned jurors must be neutral1, neutral2, neutral3
+        self.assertEqual(assigned_user_ids, {neutral1.id, neutral2.id, neutral3.id})
+
+    def test_assigned_juror_view_access(self):
+        # Create 3 neutral users for juror assignment
+        for i in range(3):
+            u = User.objects.create_user(username=f'juror_candidate_{i}', password='password123')
+            UserProfile.objects.create(user=u, rewards=500)
+
+        self.client.login(username='taker_user', password='password123')
+        self.client.post(
+            reverse('raise_dispute', args=[self.task.id]),
+            {'reason': 'View access dispute'}
+        )
+
+        dispute = Dispute.objects.get(task=self.task)
+        assigned_jurors = [a.juror for a in dispute.juror_assignments.all()]
+        self.assertEqual(len(assigned_jurors), 3)
+
+        # Create a stranger user AFTER dispute was raised so stranger is not assigned as juror
+        stranger = User.objects.create_user(username='stranger', password='password123')
+        UserProfile.objects.create(user=stranger, rewards=500)
+
+        # Stranger attempts to view dispute details
+        self.client.login(username='stranger', password='password123')
+        stranger_response = self.client.get(reverse('dispute_detail', args=[dispute.id]))
+        # Access denied -> redirected to home
+        self.assertRedirects(stranger_response, reverse('home'))
+
+        # Assigned juror attempts to view dispute details
+        assigned_juror = assigned_jurors[0]
+        self.client.login(username=assigned_juror.username, password='password123')
+        juror_response = self.client.get(reverse('dispute_detail', args=[dispute.id]))
+        self.assertEqual(juror_response.status_code, 200)
+        self.assertContains(juror_response, "Dispute Details")
+
+
--- basic/views/dispute.py
+++ basic/views/dispute.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.db import transaction
 from ..models import Dispute, Task, Notification, RewardLedger
+from ..services.juror_service import assign_jurors_for_dispute
 from django.views.decorators.http import require_POST
 from django.urls import reverse
 
@@ -10,7 +11,8 @@ from django.urls import reverse
 def dispute_detail_view(request, dispute_id):
     dispute = get_object_or_404(Dispute, id=dispute_id)
     task = dispute.task
-    if request.user != task.posted_by and request.user != task.taken_by and not request.user.is_staff:
+    is_assigned_juror = dispute.juror_assignments.filter(juror=request.user).exists()
+    if request.user != task.posted_by and request.user != task.taken_by and not request.user.is_staff and not is_assigned_juror:
         messages.error(request, "You are not authorized to view this dispute.")
         return redirect('home')
     context = {
@@ -62,6 +64,8 @@ def raise_dispute(request, task_id):
                     deposit_amount=deposit_amount,
                     escrow_status='held'
                 )
+
+            assign_jurors_for_dispute(dispute)
 
             RewardLedger.objects.create(
                 user=request.user,
